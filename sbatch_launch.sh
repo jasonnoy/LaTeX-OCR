476dab3654dca8bfe0d001a8ed180797ac964bc7
Junhui Ji <junhui.ji@aminer.cn>
2023-08-31 10:51:18 +0800

    update new filter

--- sbatch_launch.sh
+++ sbatch_launch.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=extract_math
 #SBATCH --output=./logs/extract_math%j.out
 #SBATCH --error=./logs/extract_math%j.err
-#SBATCH --nodes=16
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=3
 #SBATCH --exclude=g0001
---
+++ util.py
@@ -0,0 +1,55 @@
+import os
+import math
+from tqdm import tqdm
+from multiprocessing import Process
+
+
+def split_list_by_n(origin_list, n):
+    step = math.ceil(len(origin_list) / n)
+    res = []
+    for i in range(0, len(origin_list), step):
+        res.append(origin_list[i:i + step])
+    return res
+
+
+def write_to_file(texes, part):
+    output_path = f"/nxchinamobile2/shared/img_datasets/math_ocr/AMiner/all_parts_new/part_{part}.txt"
+    with open(output_path, "w") as f:
+        for tex in texes:
+            f.write(tex + "\n")
+    f.close()
+
+
+def filter_and_save():
+    grand_texes = {'0'}
+    # 获取当前目录
+    base_path = "/nxchinamobile2/shared/img_datasets/math_ocr/AMiner/all_new"
+    for dir in os.listdir(base_path):
+        print("processing ", dir)
+        dir_path = os.path.join(base_path, dir)
+        for filename in tqdm(os.listdir(dir_path)):
+            file_path = os.path.join(dir_path, filename)
+            with open(file_path, "r", encoding="utf-8") as f:
+                for line in f:
+                    if len(line) < 3:
+                        continue
+                    line = line.strip()
+                    grand_texes.add(line)
+    print("total texes:", len(grand_texes))
+    grand_texes = list(grand_texes)
+    splited_texes = split_list_by_n(grand_texes, 100)
+
+    print("start writing to file...")
+    process_list = []
+    for i, texes in enumerate(splited_texes):
+        p = Process(target=write_to_file, args=(texes, i))
+        p.start()
+        process_list.append(p)
+        if len(process_list) >= 32:
+            for p in process_list:
+                p.join()
+            process_list = []
+
+
+if __name__ == "__main__":
+    filter_and_save()
